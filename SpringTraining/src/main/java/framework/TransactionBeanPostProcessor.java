f547acd216f074ba23862846af4841ab4f16a860
amelchanka <amelchanka1@gmail.com>
2013-12-14 10:18:40 +0200

    Solution #2. create property map
    if class's method contains annotation put original class in map before init

--- SpringTraining/src/main/java/framework/TransactionBeanPostProcessor.java
+++ SpringTraining/src/main/java/framework/TransactionBeanPostProcessor.java
@@ -6,30 +6,37 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TransactionBeanPostProcessor implements BeanPostProcessor {
+    private Map<String, Class> map = new HashMap<String, Class>();
+
     @Override
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        Class<?> originalClass = o.getClass();
+        Method[] methods = originalClass.getMethods();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Transaction.class)) {
+                map.put(s, originalClass);
+                break;
+            }
+        }
         return o;
     }
 
     @Override
     public Object postProcessAfterInitialization(final Object o, String s) throws BeansException {
-        Class originalClass = o.getClass();
-        Method[] methods = originalClass.getMethods();
-        boolean transactionFound = false;
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(Transaction.class)) {
-                transactionFound = true;
-                break;
-            }
+        final Class<?> originalClass = map.get(s);
+        if (originalClass == null) {
+            return o;
         }
 
-        if (transactionFound) {
             Object proxy = Proxy.newProxyInstance(originalClass.getClassLoader(), originalClass.getInterfaces(), new InvocationHandler() {
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    Method originalMethod = o.getClass().getMethod(method.getName(), method.getParameterTypes());
+                    Method originalMethod = originalClass.getMethod(method.getName(), method.getParameterTypes());
                     if (originalMethod.isAnnotationPresent(Transaction.class)) {
                         System.out.println("Start");
                         Object retVal = method.invoke(o, args);
@@ -41,8 +48,5 @@ public class TransactionBeanPostProcessor implements BeanPostProcessor {
                 }
             });
             return proxy;
-        } else {
-            return o;
-        }
     }
 }
